2b6ae5d566054eff0f9213598391fe4bec3c7dd4
Tavo López Hernández <61797363+ovatlh@users.noreply.github.com>
2021-04-13 18:30:30 -0500

    part 0.2

--- js/tCarousel.js
+++ js/tCarousel.js
@@ -10,16 +10,77 @@ const tCarousel = {
     // }, time);
   },
   btnLeft(parentID) {
-    console.log(`left: ${parentID}`);
+    console.log(`left reverse: ${parentID}`);
+    var items = document.querySelectorAll(`#${parentID} .t-items .t-item`);
+    var itemActive = document.querySelector(`#${parentID} .t-items .t-active`);
+    var totalItems = items.length;
+    
+    var activeIndex = 0;
+    
+    if(!itemActive){
+      activeIndex = 0;
+    } else {
+      activeIndex = Array.from(items).indexOf(itemActive);
+    }
+
+    items.forEach(item => {
+      item.classList = "";
+      item.classList.add("t-item");
+    });
+
+    items[activeIndex].classList.add("t-out-prev-last-active");
+
+    var prevIndex = totalItems - 1;
+    if(activeIndex > 0){
+      prevIndex = activeIndex - 1;
+    }
+    items[prevIndex].classList.add("t-active","t-in-prev-exact-active");
+
+    // items[activeIndex].classList.add("t-out-prev");
+    
+    // var nextIndex = 0;
+    // if(activeIndex < totalItems - 1){
+    //   var nextIndex = activeIndex + 1;
+    // }
+    // items[nextIndex].classList.add("t-active","t-in-next");
+
+    // console.log("Active: ", activeIndex, "| Next: ", nextIndex, "| Total: ", totalItems);
+    console.log("Active: ", activeIndex, "| Total: ", totalItems);
   },
   btnRight(parentID) {
-    console.log(`right: ${parentID}`);
+    console.log(`right normal: ${parentID}`);
+    var items = document.querySelectorAll(`#${parentID} .t-items .t-item`);
+    var itemActive = document.querySelector(`#${parentID} .t-items .t-active`);
+    var totalItems = items.length;
+
+    var activeIndex = 0;
+    
+    if(!itemActive){
+      activeIndex = 0;
+    } else {
+      activeIndex = Array.from(items).indexOf(itemActive);
+    }
+
+    items.forEach(item => {
+      item.classList = "";
+      item.classList.add("t-item");
+    });
+
+    items[activeIndex].classList.add("t-out-next-last-active");
+    
+    var nextIndex = 0;
+    if(activeIndex < totalItems - 1){
+      var nextIndex = activeIndex + 1;
+    }
+    items[nextIndex].classList.add("t-active","t-in-next-exact-active");
+
+    console.log("Active: ", activeIndex, "| Next: ", nextIndex, "| Total: ", totalItems);
   },
   btnUp(parentID) {
-    console.log(`up: ${parentID}`);
+    console.log(`up reverse: ${parentID}`);
   },
   btnDown(parentID) {
-    console.log(`down: ${parentID}`);
+    console.log(`down normal: ${parentID}`);
   },
   btnEvents() {
     const btnsLeft = document.querySelectorAll(".t-left");
@@ -50,7 +111,7 @@ const tCarousel = {
   },
   init() {
     var tCarouselContainers = document.querySelectorAll(".t-carousel");
-    
+
     tCarouselContainers.forEach(function (value, index, array) {
       if (value?.id) {
         const tItemsContainer = document.querySelector(`#${value.id} .t-items`);
@@ -58,7 +119,7 @@ const tCarousel = {
         const itemActive = document.querySelector(
           `#${value.id} .t-items .t-active`
         );
-        if (!itemActive && tItemsContainer.childElementCount > 0) {
+        if (!itemActive && tItemsContainer?.childElementCount > 0) {
           tItemsContainer.children[0].classList.add("t-active");
         }
 
