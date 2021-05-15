4ebf73acfa761f23d38616f6ac8dc29ab81dbe1c
Tavo López Hernández <61797363+ovatlh@users.noreply.github.com>
2021-05-15 18:25:15 -0500

    tCarousel v4.0.0
    options
    tInit: show_Messages | default = false, values [true,false]
    tClear: only_Automatic | default = false, values [true, false]
    
    tInit( { show_Messages: true } ) | Shows init/error messages
    tClear( { only_Automatic: true } ) | Delete only interval
    tClear( { only_Automatic: false } ) | Delete interval and click events

--- js/tCarousel.js
+++ js/tCarousel.js
@@ -1,11 +1,12 @@
 const tCarousel = {
   // tCarousel
-  class_tBTN: "t-btn",
+  show_Messages: false,
   class_tCarousel: "t-carousel",
   class_tActive: "t-active",
   class_tItems: "t-items",
   class_tItem: "t-item",
   // Buttons
+  class_tBTN: "t-btn",
   class_Btn_Left: "t-left",
   class_Btn_Right: "t-right",
   class_Btn_Up: "t-up",
@@ -28,165 +29,187 @@ const tCarousel = {
   intervalsObj: {},
   tAutomatic(id, time, direction) {
     if (!tCarousel.intervalsObj[`${id}`]) {
-      console.log("tCarousel - Automatic: ", id, time, direction);
+      if (tCarousel.show_Messages) {
+        console.log("tCarousel - Automatic: ", id, time, direction);
+      }
       tCarousel.intervalsObj[`${id}`] = setInterval(() => {
         tCarousel.tBTN(id, direction);
       }, time);
     } else {
-      console.log("tCarousel - Running: ", id, time, direction);
+      if (tCarousel.show_Messages) {
+        console.log("tCarousel - Running: ", id, time, direction);
+      }
     }
   },
   tBTN(id, direction) {
-    var items = document.querySelectorAll(
-      `#${id} .${tCarousel.class_tItems} .${tCarousel.class_tItem}`
-    );
-    var item_active = document.querySelector(
-      `#${id} .${tCarousel.class_tItems} .${tCarousel.class_tActive}`
-    );
-    var total_items = items.length;
-    if (total_items <= 1) {
-      console.log("---------------------------------------");
-      console.log(document.getElementById(id));
-      console.error(
-        `tCarousel: #${id} - direction: ${direction}\n${tCarousel.error_NoItems}\n${tCarousel.error_Min2Items}`
+    try {
+      var items = document.querySelectorAll(
+        `#${id} .${tCarousel.class_tItems} .${tCarousel.class_tItem}`
       );
-      console.log("---------------------------------------");
-      return;
-    }
-
-    var active_index = 0;
-    if (item_active) {
-      active_index = Array.from(items).indexOf(item_active);
-    }
-
-    items.forEach((item) => {
-      item.classList = "";
-      item.classList.add(`${tCarousel.class_tItem}`);
-    });
-
-    var class_result_out = "";
-    switch (direction) {
-      case "left":
-        class_result_out = `${tCarousel.class_t_H_OUT_Right}`;
-        break;
-      case "right":
-        class_result_out = `${tCarousel.class_t_H_OUT_Left}`;
-        break;
-      case "up":
-        class_result_out = `${tCarousel.class_t_V_OUT_Down}`;
-        break;
-      case "down":
-        class_result_out = `${tCarousel.class_t_V_OUT_Up}`;
-        break;
-      default:
-        class_result_out = `${tCarousel.class_t_H_OUT_Right}`;
-        break;
-    }
-    items[active_index].classList.add(class_result_out);
-
-    //Is Horizontal
-    if (direction === "left" || direction === "right") {
-      //Is left
-      if (direction === "left") {
-        var prev_index = total_items - 1;
-        if (active_index > 0) {
-          prev_index = active_index - 1;
+      var item_active = document.querySelector(
+        `#${id} .${tCarousel.class_tItems} .${tCarousel.class_tActive}`
+      );
+      var total_items = items.length;
+      if (total_items <= 1) {
+        if (tCarousel.show_Messages) {
+          console.log("---------------------------------------");
+          console.log(document.getElementById(id));
+          console.error(
+            `tCarousel: #${id} - direction: ${direction}\n${tCarousel.error_NoItems}\n${tCarousel.error_Min2Items}`
+          );
+          console.log("---------------------------------------");
+          return;
         }
-
-        items[prev_index].classList.add(
-          `${tCarousel.class_tActive}`,
-          `${tCarousel.class_t_H_IN_Left}`
-        );
       }
-      //Is Right
+
+      var active_index = 0;
+      if (item_active) {
+        active_index = Array.from(items).indexOf(item_active);
+      }
+
+      items.forEach((item) => {
+        item.classList = "";
+        item.classList.add(`${tCarousel.class_tItem}`);
+      });
+
+      var class_result_out = "";
+      switch (direction) {
+        case "left":
+          class_result_out = `${tCarousel.class_t_H_OUT_Right}`;
+          break;
+        case "right":
+          class_result_out = `${tCarousel.class_t_H_OUT_Left}`;
+          break;
+        case "up":
+          class_result_out = `${tCarousel.class_t_V_OUT_Down}`;
+          break;
+        case "down":
+          class_result_out = `${tCarousel.class_t_V_OUT_Up}`;
+          break;
+        default:
+          class_result_out = `${tCarousel.class_t_H_OUT_Right}`;
+          break;
+      }
+      items[active_index].classList.add(class_result_out);
+
+      //Is Horizontal
+      if (direction === "left" || direction === "right") {
+        //Is left
+        if (direction === "left") {
+          var prev_index = total_items - 1;
+          if (active_index > 0) {
+            prev_index = active_index - 1;
+          }
+
+          items[prev_index].classList.add(
+            `${tCarousel.class_tActive}`,
+            `${tCarousel.class_t_H_IN_Left}`
+          );
+        }
+        //Is Right
+        else {
+          var next_index = 0;
+          if (active_index < total_items - 1) {
+            next_index = active_index + 1;
+          }
+
+          items[next_index].classList.add(
+            `${tCarousel.class_tActive}`,
+            `${tCarousel.class_t_H_IN_Right}`
+          );
+        }
+      }
+
+      //Is Vertical
       else {
-        var next_index = 0;
-        if (active_index < total_items - 1) {
-          next_index = active_index + 1;
-        }
+        //Is Up
+        if (direction === "up") {
+          var prev_index = total_items - 1;
+          if (active_index > 0) {
+            prev_index = active_index - 1;
+          }
 
-        items[next_index].classList.add(
-          `${tCarousel.class_tActive}`,
-          `${tCarousel.class_t_H_IN_Right}`
-        );
+          items[prev_index].classList.add(
+            `${tCarousel.class_tActive}`,
+            `${tCarousel.class_t_V_IN_Up}`
+          );
+        }
+        //Is Down
+        else {
+          var next_index = 0;
+          if (active_index < total_items - 1) {
+            next_index = active_index + 1;
+          }
+
+          items[next_index].classList.add(
+            `${tCarousel.class_tActive}`,
+            `${tCarousel.class_t_V_IN_Down}`
+          );
+        }
       }
-    }
-
-    //Is Vertical
-    else {
-      //Is Up
-      if (direction === "up") {
-        var prev_index = total_items - 1;
-        if (active_index > 0) {
-          prev_index = active_index - 1;
-        }
-
-        items[prev_index].classList.add(
-          `${tCarousel.class_tActive}`,
-          `${tCarousel.class_t_V_IN_Up}`
-        );
-      }
-      //Is Down
-      else {
-        var next_index = 0;
-        if (active_index < total_items - 1) {
-          next_index = active_index + 1;
-        }
-
-        items[next_index].classList.add(
-          `${tCarousel.class_tActive}`,
-          `${tCarousel.class_t_V_IN_Down}`
-        );
+    } catch (error) {
+      if (tCarousel.show_Messages) {
+        console.error(error);
       }
     }
   },
   tBTNExtra(event) {
     var res_tC = event.target.closest(`.${tCarousel.class_tCarousel}`);
     var btn = event.target.closest(`.${tCarousel.class_tBTN}`);
-    
-    if(btn.classList.contains(`${tCarousel.class_Btn_Left}`)) {
+
+    if (btn.classList.contains(`${tCarousel.class_Btn_Left}`)) {
       tCarousel.tBTN(res_tC.id, "left");
     }
-    if(btn.classList.contains(`${tCarousel.class_Btn_Right}`)) {
+    if (btn.classList.contains(`${tCarousel.class_Btn_Right}`)) {
       tCarousel.tBTN(res_tC.id, "right");
     }
-    if(btn.classList.contains(`${tCarousel.class_Btn_Up}`)) {
+    if (btn.classList.contains(`${tCarousel.class_Btn_Up}`)) {
       tCarousel.tBTN(res_tC.id, "up");
     }
-    if(btn.classList.contains(`${tCarousel.class_Btn_Down}`)) {
+    if (btn.classList.contains(`${tCarousel.class_Btn_Down}`)) {
       tCarousel.tBTN(res_tC.id, "down");
     }
   },
-  tBTN_ClickEvents() {
+  tBTNsClear() {
     const btns_Left = document.querySelectorAll(`.${tCarousel.class_Btn_Left}`);
-    const btns_Right = document.querySelectorAll(`.${tCarousel.class_Btn_Right}`);
+    const btns_Right = document.querySelectorAll(
+      `.${tCarousel.class_Btn_Right}`
+    );
     const btns_Up = document.querySelectorAll(`.${tCarousel.class_Btn_Up}`);
     const btns_Down = document.querySelectorAll(`.${tCarousel.class_Btn_Down}`);
-    
     btns_Left.forEach((button) => {
       button.removeEventListener("click", tCarousel.tBTNExtra);
     });
+    btns_Right.forEach((button) => {
+      button.removeEventListener("click", tCarousel.tBTNExtra);
+    });
+    btns_Up.forEach((button) => {
+      button.removeEventListener("click", tCarousel.tBTNExtra);
+    });
+    btns_Down.forEach((button) => {
+      button.removeEventListener("click", tCarousel.tBTNExtra);
+    });
+  },
+  tBTN_ClickEvents() {
+    const btns_Left = document.querySelectorAll(`.${tCarousel.class_Btn_Left}`);
+    const btns_Right = document.querySelectorAll(
+      `.${tCarousel.class_Btn_Right}`
+    );
+    const btns_Up = document.querySelectorAll(`.${tCarousel.class_Btn_Up}`);
+    const btns_Down = document.querySelectorAll(`.${tCarousel.class_Btn_Down}`);
+
     btns_Left.forEach((button) => {
       button.addEventListener("click", tCarousel.tBTNExtra);
     });
 
     btns_Right.forEach((button) => {
-      button.removeEventListener("click", tCarousel.tBTNExtra);
-    });
-    btns_Right.forEach((button) => {
       button.addEventListener("click", tCarousel.tBTNExtra);
     });
-    
-    btns_Up.forEach((button) => {
-      button.removeEventListener("click", tCarousel.tBTNExtra);
-    });
+
     btns_Up.forEach((button) => {
       button.addEventListener("click", tCarousel.tBTNExtra);
     });
-    
-    btns_Down.forEach((button) => {
-      button.removeEventListener("click", tCarousel.tBTNExtra);
-    });
+
     btns_Down.forEach((button) => {
       button.addEventListener("click", tCarousel.tBTNExtra);
     });
@@ -216,12 +239,29 @@ const tCarousel = {
           );
         }
       } else {
-        console.error(`${tCarousel.error_tCNoID}`);
+        if (tCarousel.show_Messages) {
+          console.error(`${tCarousel.error_tCNoID}`);
+        }
       }
     });
   },
-  tInit() {
+  tClear(options = { only_Automatic: false }) {
+    if (!options.only_Automatic) {
+      tCarousel.tBTNsClear();
+    }
+    Object.keys(tCarousel.intervalsObj).forEach((e) => {
+      clearInterval(tCarousel.intervalsObj[e]);
+      tCarousel.intervalsObj[e] = null;
+      if (tCarousel.show_Messages) {
+        console.log(`tCarousel | CLEAR: ${e}`);
+      }
+    });
+  },
+  tInit(options = { show_Messages: false }) {
+    tCarousel.show_Messages = options.show_Messages;
+
     tCarousel.tSetup();
+    tCarousel.tBTNsClear();
     tCarousel.tBTN_ClickEvents();
   },
 };
